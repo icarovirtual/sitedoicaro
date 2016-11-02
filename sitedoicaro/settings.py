0000654022d51b5bce8a917a9be0e2060af83214
Icaro Raupp Henrique <icaro@aeroli.to>
2016-11-02 20:36:40 -0200

    say hello to my little website

---
+++ sitedoicaro/settings.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+
+import os
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+INSTALLED_APPS = [
+    'test_without_migrations',
+    'django_nose',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
+]
+
+ROOT_URLCONF = 'sitedoicaro.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'sitedoicaro.wsgi.application'
+
+# region Internationalization
+LANGUAGE_CODE = 'pt-br'
+
+TIME_ZONE = 'America/Sao_Paulo'
+
+USE_I18N = True
+
+USE_L10N = False
+
+USE_TZ = False
+
+DATE_INPUT_FORMATS = ['%d/%m/%Y']
+DATE_FORMAT = 'd/m/Y'
+TIME_FORMAT = 'H:i'
+DATETIME_FORMAT = 'd/m/Y H:i:s'
+# endregion
+
+# region Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# endregion
+
+# region Unit Testing
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--nologcapture',
+    '--with-coverage',
+    '--cover-package=common',
+    '--cover-package=questionario',
+    '--cover-html',
+    '--nocapture'
+]
+
+TEST_WITHOUT_MIGRATIONS_COMMAND = 'django_nose.management.commands.test.Command'
+# endregion
+
+if not os.path.exists(os.path.join(BASE_DIR, 'sitedoicaro', 'local.py')):
+    raise Exception(u"Couldn't import the local configuration file... Create a 'local.py' file in the project's main app directory.")
+else:
+    from local import *
